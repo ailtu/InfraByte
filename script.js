f9f59c309576d8203f9219d80fbaec37028f11f7
Ailton <ailton.2020202273@unicap.br>
2023-07-24 18:09:24 -0300

    Construção do corpo

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
